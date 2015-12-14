398775b53fdf336a3e6e9151f007f2a4cfaf5267
cloudOfWorld <iamkun666@163.com>
2015-12-14 21:37:30 +0800

    代码提交信息

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by yxk on 15/12/14.
+//  Copyright © 2015年 yxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by yxk on 15/12/14.
+//  Copyright © 2015年 yxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
